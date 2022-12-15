b01db4c17b3b8a95821a4137f717e0c9342d1356
Ryan Samford <rsamfordnet@gmail.com>
2022-12-15 09:42:02 -0500

    updated user role controller

--- Controllers/UserRolesController.cs
+++ Controllers/UserRolesController.cs
@@ -19,8 +19,8 @@ namespace IssueTracker.Controllers
             _companyInfoService = companyInfoService;
         }
 
-        public int ManagedUserRolesViewModel { get; private set; }
 
+        [HttpGet]
         public async Task<IActionResult> ManageUserRoles()
         {
             // Add an instance of the ViewModel as a List (model)
@@ -52,6 +52,41 @@ namespace IssueTracker.Controllers
             // Return the model to the View
             
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]  
+        public async Task<IActionResult> ManageUserRoles(ManageUserRolesViewModel member)
+        {
+
+            //Get the companyId
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            // Instantiate the BTUser
+            BTUser btUser = (await _companyInfoService.GetAllMembersAsync(companyId)).FirstOrDefault(u => u.Id == member.BTUser.Id);
+
+            // Get Roles for the User
+            IEnumerable<string> roles = await _rolesService.GetUserRolesAsync(btUser);
+
+            // Grab the selected role
+            string userRole = member.SelectedRoles.FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(userRole))
+            {
+                // Remove User from their roles
+                if(await _rolesService.RemoveUserFromRolesAsync(btUser, roles))
+                {
+                    // Add User to the new role
+                    await _rolesService.AddUserToRoleAsync(btUser, userRole);
+                }
+                
+            }
+
+
+            // Navigate back to the View
+            return RedirectToAction(nameof(ManageUserRoles));
+
+
         }
     }
 }
