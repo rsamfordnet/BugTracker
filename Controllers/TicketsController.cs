a7ff3cb0dc6963fd7a6f360976668dd5ec605099
Ryan Samford <rsamfordnet@gmail.com>
2022-12-19 12:03:12 -0500

    added archieved tickets method, controller and view

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -64,6 +64,16 @@ namespace IssueTracker.Controllers
             return View(tickets);
         }
 
+        public async Task<IActionResult> ArchivedTickets()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Ticket> tickets = await _ticketService.GetArchivedTicketsAsync(companyId);
+
+            return View(tickets);   
+        }
+
+
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
         {
